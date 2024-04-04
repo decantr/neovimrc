50c8497ecc767baad5e79223f71f4da4a5e26def
Shaw Eastwood <eastwoodshaw@gmail.com>
2024-04-04 19:15:15 +0100

    feat: disable css lint for tailwind

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,18 +1,54 @@
 return {
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = function()
-        -- don't use termguicolors on Apple Terminal
-        local opt = vim.opt
+	{
+		"neovim/nvim-lspconfig",
+		---@class PluginLspOpts
+		opts = {
+			---@type lspconfig.options
+			servers = {
+				-- intelephense will be automatically installed with mason and loaded with lspconfig
+				intelephense = {},
+				cssls = {
+					settings = {
+						css = {
+							validate = true,
+							lint = {
+								unknownAtRules = "ignore"
+							}
+						},
+						scss = {
+							validate = true,
+							lint = {
+								unknownAtRules = "ignore"
+							}
+						},
+						less = {
+							validate = true,
+							lint = {
+								unknownAtRules = "ignore"
+							}
+						},
+					},
 
-        if os.getenv("TERM_PROGRAM") == "Apple_Terminal" then
-          opt.termguicolors = false
-          return "default"
-        end
-
-        return "tokyonight"
-      end,
-    },
-  },
+				}
+			},
+		},
+	},
 }
+--return {
+--  {
+--    "LazyVim/LazyVim",
+--    opts = {
+--      colorscheme = function()
+--        -- don't use termguicolors on Apple Terminal
+--        local opt = vim.opt
+--
+--        if os.getenv("TERM_PROGRAM") == "Apple_Terminal" then
+--          opt.termguicolors = false
+--          return "default"
+--        end
+--
+--        return "tokyonight"
+--      end,
+--    },
+--  },
+--}
