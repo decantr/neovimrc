7d6e2bc4f40b8796b9836d3f1a6e7645fafdfc29
Shaw Eastwood <eastwoodshaw@gmail.com>
2024-05-16 21:16:18 +0100

    feat(plugin): remove auto dark
    turns out this is being handled by neovide, and it
     wasnt even set up right anyway!

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,20 +1,6 @@
 return {
 	{ 'projekt0n/github-nvim-theme' },
 	{ 'p00f/alabaster.nvim' },
-	{
-		'f-person/auto-dark-mode.nvim',
-		config = {
-			-- update_interval = 3000,
-			set_dark_mode = function()
-				vim.api.nvim_set_option('background', 'dark')
-				-- vim.cmd('colorscheme alabaster')
-				end,
-			set_light_mode = function()
-				vim.api.nvim_set_option('background', 'light')
-				-- vim.cmd('colorscheme alabaster')
-				end,
-		},
-	},
 
 	-- Configure LazyVim to load gruvbox
 	{
