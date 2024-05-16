40b4528b59ea273f7d4fe40bcda7ebdb52c5df52
Shaw Eastwood <eastwoodshaw@gmail.com>
2024-05-16 21:22:41 +0100

    chore: enforce tabs

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,13 +2,13 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 if vim.g.neovide then
-    -- Put anything you want to happen only in Neovide here
+		-- Put anything you want to happen only in Neovide here
 	vim.g.neovide_theme = 'auto'
 end
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "*" },
-  callback = function()
-    vim.b.autoformat = false
-  end,
+	pattern = { "*" },
+	callback = function()
+		vim.b.autoformat = false
+	end,
 })
--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -35,20 +35,20 @@ return {
 	},
 }
 --return {
---  {
---    "LazyVim/LazyVim",
---    opts = {
---      colorscheme = function()
---        -- don't use termguicolors on Apple Terminal
---        local opt = vim.opt
+--	{
+--		"LazyVim/LazyVim",
+--		opts = {
+--			colorscheme = function()
+--				-- don't use termguicolors on Apple Terminal
+--				local opt = vim.opt
 --
---        if os.getenv("TERM_PROGRAM") == "Apple_Terminal" then
---          opt.termguicolors = false
---          return "default"
---        end
+--				if os.getenv("TERM_PROGRAM") == "Apple_Terminal" then
+--					opt.termguicolors = false
+--					return "default"
+--				end
 --
---        return "tokyonight"
---      end,
---    },
---  },
+--				return "tokyonight"
+--			end,
+--		},
+--	},
 --}
